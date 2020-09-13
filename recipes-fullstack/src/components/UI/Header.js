ed123a284716d316a4e15b4f07f15f4317ba95d9
Josh Garcia <josh2397garcia@gmail.com>
2020-09-13 13:17:26 -0400

    Add floating elevation effect to AppBar Header

--- recipes-fullstack/src/components/UI/Header.js
+++ recipes-fullstack/src/components/UI/Header.js
@@ -2,21 +2,36 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Button from '@material-ui/core/Button';
+
+const ElevationScroll = props => {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
 
 const header = props => {
     return (
-        <AppBar position='fixed'>
-            <Toolbar>
-                <Typography variant='h4'>
-                    Good Food
-                </Typography>
-                {/* <Button>Home</Button>
-                <Button>My Recipes</Button>
-                <Button>My Profile</Button>
-                <Button>Login</Button> */}
-            </Toolbar>
-        </AppBar>
+        <ElevationScroll>
+            <AppBar position='fixed'>
+                <Toolbar>
+                    <Typography variant='h4'>
+                        Good Food
+                    </Typography>
+                    {/* <Button>Home</Button>
+                    <Button>My Recipes</Button>
+                    <Button>My Profile</Button>
+                    <Button>Login</Button> */}
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
     );
 };
 
