9b53cead61b9efc35316b4f3434e7fb095bb2baf
Josh Garcia <josh2397garcia@gmail.com>
2020-09-13 16:43:40 -0400

    Setup initial Tabs in AppBar

--- recipes-fullstack/src/components/UI/Header.js
+++ recipes-fullstack/src/components/UI/Header.js
@@ -1,38 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+	toolbarMargin: {
+		...theme.mixins.toolbar,
+	},
+	tabContainer: {
+		marginLeft: 'auto',
+	},
+	tab: {
+		minWidth: 15,
+        marginLeft: '15px',
+        marginRight: '15px',
+	},
+}));
 
 const ElevationScroll = props => {
-    const { children } = props;
-    const trigger = useScrollTrigger({
-      disableHysteresis: true,
-      threshold: 0
-    });
-  
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
-  }
+	const { children } = props;
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+	});
 
-const header = props => {
-    return (
-        <ElevationScroll>
-            <AppBar position='fixed' color='primary'>
-                <Toolbar>
-                    <Typography variant='h3'>
-                        Good Food
-                    </Typography>
-                    {/* <Button>Home</Button>
-                    <Button>My Recipes</Button>
-                    <Button>My Profile</Button>
-                    <Button>Login</Button> */}
-                </Toolbar>
-            </AppBar>
-        </ElevationScroll>
-    );
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0,
+	});
 };
 
-export default header;
+const Header = props => {
+	const classes = useStyles();
+	const [tabValue, setTabValue] = useState(0);
+
+	const handleTabChange = (event, newValue) => {
+		setTabValue(newValue);
+	};
+
+	const routes = [
+		{ name: 'Home', link: '/' },
+		{ name: 'My Recipes', link: '/recipes' },
+		{ name: 'My Profile', link: '/profile' },
+		{ name: 'Log In/Out', link: '/auth' },
+	];
+
+	const tabs = (
+		<Tabs
+			className={classes.tabContainer}
+			value={tabValue}
+            onChange={handleTabChange}
+            /*indicatorColor='primary'*/
+		>
+			{routes.map((route, index) => (
+				<Tab
+					className={classes.tab}
+					key={`${route}${index}`}
+					label={route.name}
+				/>
+			))}
+		</Tabs>
+	);
+
+	return (
+		<React.Fragment>
+			<ElevationScroll>
+				<AppBar position='fixed' color='primary'>
+					<Toolbar>
+						<Typography variant='h3'>Good Food</Typography>
+						{tabs}
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<div className={classes.toolbarMargin} />
+		</React.Fragment>
+	);
+};
+
+export default Header;
