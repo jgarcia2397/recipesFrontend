46bb3afd59557e26f84d7db0f4c0ae8dbf86f2ea
Josh Garcia <josh2397garcia@gmail.com>
2020-09-20 13:52:01 -0400

    Use Higher-order component API in HomePage

--- recipes-fullstack/src/containers/HomePage.js
+++ recipes-fullstack/src/containers/HomePage.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+
+const styles = {};
+
+// Higher-order component API
 class HomePage extends Component {
-    render () {
-        return (
-            <div>This is the HomePage</div>
-        );
-    }
+	render() {
+		const { classes } = this.props;
+		return <div>This is the HomePage</div>;
+	}
 }
 
-export default HomePage;
+HomePage.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(HomePage);
