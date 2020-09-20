55f597e2edfbe590e1b56a2984d7f21723cd4ec5
Josh Garcia <josh2397garcia@gmail.com>
2020-09-20 14:56:04 -0400

    Create separate SearchBar component

--- recipes-fullstack/src/containers/HomePage.js
+++ recipes-fullstack/src/containers/HomePage.js
@@ -2,27 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
+
+import SearchBar from '../components/UI/SearchBar';
 
 const styles = {
-    root: {
-        elevation: 5,
-        width: 1000,
-        padding: '3px 6px',
-        margin: '300px auto',
-        display: 'flex'
-    },
-    searchIconContainer: {
-        '&:hover': {
-			backgroundColor: 'transparent',
-		},
-    },
-    input: {
-        flex: 1
-    }
+    
 };
 
 // Higher-order component API
@@ -30,12 +14,7 @@ class HomePage extends Component {
 	render() {
 		const { classes } = this.props;
 		return (
-            <Paper component='form' className={classes.root}>
-                <IconButton type='submit' className={classes.searchIconContainer}>
-                    <SearchIcon />
-                </IconButton>
-                <InputBase placeholder='Search users or recipes' className={classes.input} />
-            </Paper>
+            <SearchBar />
         );
 	}
 }
