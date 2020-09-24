1c37b7171a519f7be8b06439e31dc4aa488d3786
Josh Garcia <josh2397garcia@gmail.com>
2020-09-24 18:48:29 -0400

    Add initial background for RecipePage

--- recipes-fullstack/src/containers/RecipePage.js
+++ recipes-fullstack/src/containers/RecipePage.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+	root: {
+        height: '95vh',
+        display: 'flex',
+	},
+	background: {
+		backgroundColor: theme.palette.primary.light,
+		height: '100%',
+        width: '100%',
+        overflow: 'auto'
+	},
+});
 
 class RecipePage extends Component {
-    render () {
-        return (
-            <div>This is the RecipePage</div>
-        );
-    }
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className={classes.root}>
+				<Paper className={classes.background} square></Paper>
+			</div>
+		);
+	}
 }
 
-export default RecipePage;
+RecipePage.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipePage);
