3e39444f06e7969484a4360191a67731b0817aec
Josh Garcia <josh2397garcia@gmail.com>
2020-09-24 19:09:56 -0400

    Add NewRecipe Button on RecipePage

--- recipes-fullstack/src/containers/RecipePage.js
+++ recipes-fullstack/src/containers/RecipePage.js
@@ -3,17 +3,31 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
 	root: {
-        height: '95vh',
-        display: 'flex',
+		height: '95vh',
+		display: 'flex',
 	},
 	background: {
 		backgroundColor: theme.palette.primary.light,
 		height: '100%',
-        width: '100%',
-        overflow: 'auto'
+		width: '100%',
+		overflow: 'auto',
+	},
+	newRecipeButtonContainer: {
+		margin: '40px auto',
+	},
+	newRecipeButton: {
+		...theme.typography.button,
+		color: 'white',
+		backgroundColor: theme.palette.secondary.main,
+		borderRadius: 50,
+		'&:hover': {
+			backgroundColor: theme.palette.secondary.dark,
+		},
 	},
 });
 
@@ -22,7 +36,19 @@ class RecipePage extends Component {
 		const { classes } = this.props;
 		return (
 			<div className={classes.root}>
-				<Paper className={classes.background} square></Paper>
+				<Paper className={classes.background} square>
+					<Grid container direction='column'>
+						<Grid item className={classes.newRecipeButtonContainer}>
+							<Button
+								className={classes.newRecipeButton}
+								style={{ maxWidth: '150px', minWidth: '150px' }}
+							>
+								New Recipe
+							</Button>
+						</Grid>
+						<Grid item></Grid>
+					</Grid>
+				</Paper>
 			</div>
 		);
 	}
