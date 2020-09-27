c2c4d5a9489da450eae7416b220a35dcbf928986
Josh Garcia <josh2397garcia@gmail.com>
2020-09-27 14:01:26 -0400

    Responsive design for RecipeInfoColumn

--- recipes-fullstack/src/components/Pages/RecipeFullDetailsPage.js
+++ recipes-fullstack/src/components/Pages/RecipeFullDetailsPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 
@@ -11,25 +12,42 @@ const useStyles = makeStyles(theme => ({
 	root: {
 		height: '95vh',
 		width: '100vw',
+		[theme.breakpoints.down('md')]: {
+			height: '150vh',
+		},
 	},
 	infoColumn: {
 		width: '25.2%',
 		height: '100%',
 		backgroundColor: theme.palette.primary.light,
+		[theme.breakpoints.down('md')]: {
+			width: '100%',
+			height: '33.4%',
+		},
 	},
 	instructColumn: {
 		width: '37.3%',
 		height: '100%',
 		backgroundColor: theme.palette.secondary.light,
+		[theme.breakpoints.down('md')]: {
+			width: '100%',
+			height: '33.2%',
+		},
 	},
 	divider: {
 		backgroundColor: theme.divider.main,
 		width: '0.1%',
+		[theme.breakpoints.down('md')]: {
+			width: '100%',
+			height: '0.1%',
+		},
 	},
 }));
 
 const RecipeFullDetailsPage = props => {
 	const classes = useStyles();
+	const theme = useTheme();
+	const matchesMD = useMediaQuery(theme.breakpoints.down('md'));
 
 	const ingredients = [
 		'1/2 cup butter',
@@ -50,18 +68,22 @@ const RecipeFullDetailsPage = props => {
 	];
 
 	return (
-		<Grid container direction='row' className={classes.root}>
+		<Grid
+			container
+			direction={matchesMD ? 'column' : 'row'}
+			className={classes.root}
+		>
 			<Grid item className={classes.infoColumn}>
 				<RecipeInfoColumn />
 			</Grid>
 			<Grid item className={classes.divider}>
-				<Divider orientation='vertical' classes={{ root: classes.divider }} />
+				<Divider orientation={matchesMD ? 'horizontal' : 'vertical'} />
 			</Grid>
 			<Grid item className={classes.instructColumn}>
 				<RecipeInstructColumn label='Ingredients' array={ingredients} />
 			</Grid>
 			<Grid item className={classes.divider}>
-				<Divider orientation='vertical' />
+				<Divider orientation={matchesMD ? 'horizontal' : 'vertical'} />
 			</Grid>
 			<Grid item className={classes.instructColumn}>
 				<RecipeInstructColumn label='Directions' array={directions} />
--- recipes-fullstack/src/components/UI/RecipeInfoColumn.js
+++ recipes-fullstack/src/components/UI/RecipeInfoColumn.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
@@ -12,11 +13,21 @@ const useStyles = makeStyles(theme => ({
 	infoContainer: {
 		width: '100%',
 		height: '100%',
-    },
-    recipeImageContainer: {
+	},
+	recipeImageContainer: {
 		marginBottom: '75px',
 		height: '20%',
 		width: '70%',
+		[theme.breakpoints.down('md')]: {
+			marginBottom: '35px',
+			height: '40%',
+			width: '35%',
+		},
+		[theme.breakpoints.down('xs')]: {
+			marginBottom: '10px',
+			height: '30%',
+			width: '30%',
+		},
 	},
 	recipeImage: {
 		backgroundImage: `url(${applePie})`,
@@ -25,26 +36,38 @@ const useStyles = makeStyles(theme => ({
 		backgroundRepeat: 'no-repeat',
 		width: '100%',
 		height: '100%',
-    },
-    recipeTitle: {
+	},
+	recipeTitle: {
 		marginBottom: '25px',
+		[theme.breakpoints.down('md')]: {
+			marginBottom: '10px',
+		},
 	},
 	info: {
 		marginBottom: '15px',
-    },
-    modifyRecipeButton: {
-        ...theme.typography.button,
-        marginTop: '25px',
+		[theme.breakpoints.down('md')]: {
+			marginBottom: '2.5px',
+		},
+	},
+	modifyRecipeButton: {
+		...theme.typography.button,
+		marginTop: '25px',
 		borderRadius: 50,
 		backgroundColor: theme.palette.secondary.main,
 		'&:hover': {
 			backgroundColor: theme.palette.secondary.dark,
 		},
+		[theme.breakpoints.down('xs')]: {
+			marginTop: '10px',
+		},
 	},
 }));
 
 const RecipeInfoColumn = props => {
-    const classes = useStyles();
+	const classes = useStyles();
+	const theme = useTheme();
+
+	const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
 
 	return (
 		<Grid
@@ -58,32 +81,32 @@ const RecipeInfoColumn = props => {
 				<Paper className={classes.recipeImage} />
 			</Grid>
 			<Grid item>
-				<Typography variant='h3' className={classes.recipeTitle}>
+				<Typography variant={matchesXS ? 'h4' : 'h3'} className={classes.recipeTitle}>
 					Apple Pie
 				</Typography>
 			</Grid>
 			<Grid item>
-				<Typography variant='body1' className={classes.info}>
+				<Typography variant={matchesXS ? 'body2' : 'body1'} className={classes.info}>
 					Prep Time: 20 mins
 				</Typography>
 			</Grid>
 			<Grid item>
-				<Typography variant='body1' className={classes.info}>
+				<Typography variant={matchesXS ? 'body2' : 'body1'} className={classes.info}>
 					Cook Time: 1 hr
 				</Typography>
 			</Grid>
 			<Grid item>
-				<Typography variant='body1' className={classes.info}>
+				<Typography variant={matchesXS ? 'body2' : 'body1'} className={classes.info}>
 					Total Time: 1 hr 20 mins
 				</Typography>
 			</Grid>
 			<Grid item>
-				<Typography variant='body1' className={classes.info}>
+				<Typography variant={matchesXS ? 'body2' : 'body1'} className={classes.info}>
 					Servings: 10
 				</Typography>
 			</Grid>
 			<Grid item>
-				<Typography variant='body1' className={classes.info}>
+				<Typography variant={matchesXS ? 'body2' : 'body1'} className={classes.info}>
 					Difficulty: Medium
 				</Typography>
 			</Grid>
