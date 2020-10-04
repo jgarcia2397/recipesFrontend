3c614d9cb83ec72b3b5aa7f63502afde3b53f74e
Josh Garcia <josh2397garcia@gmail.com>
2020-10-04 15:10:24 -0400

    Reuse RecipeInstructColumn components in CreateRecipePage and add initial Save recipe button

--- recipes-fullstack/src/components/Pages/CreateRecipePage.js
+++ recipes-fullstack/src/components/Pages/CreateRecipePage.js
@@ -4,9 +4,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 
 import BasicRecipeInfoInputs from '../UI/BasicRecipeInfoInputs';
 import ImageUpload from '../UI/ImageUpload';
+import RecipeInstructColumn from '../UI/RecipeInstructColumn';
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -27,15 +29,52 @@ const useStyles = makeStyles(theme => ({
 		// 	height: '0.1%',
 		// },
 	},
+	instructColumn: {
+		width: '49.97%',
+		backgroundColor: theme.palette.secondary.light,
+		// [theme.breakpoints.down('md')]: {
+		// 	width: '100%',
+		// 	height: '33.2%',
+		// },
+	},
+	recipeDetailsRoot: {
+		width: '100%',
+	},
+	recipeDetailsContainer: {
+		height: '45vh',
+	},
+	saveRecipeButton: {
+		...theme.typography.button,
+		marginTop: '15px',
+		borderRadius: 50,
+		backgroundColor: theme.palette.secondary.main,
+		'&:hover': {
+			backgroundColor: theme.palette.secondary.dark,
+		},
+    },
 }));
 
 const CreateRecipePage = props => {
 	const classes = useStyles();
 
+	const ingredients = [
+		'1/2 cup butter',
+		'3 tablespoons flour',
+		'1/4 cup water',
+		'1/2 cup sugar',
+		'6 apples',
+	];
+	const directions = [
+		'Preheat oven to 350 degrees. Preheat oven to 350 degrees. Preheat oven to 350 degrees. Preheat oven to 350 degrees. Preheat oven to 350 degrees. Preheat oven to 350 degrees.',
+		'Make crust',
+		'Start peeling and slicing apples',
+		'Make crust',
+	];
+
 	return (
 		<div className={classes.root}>
 			<Paper className={classes.background} square>
-				<Grid container direction='column'>
+				<Grid container direction='column' alignItems='center'>
 					<Grid item>
 						<ImageUpload />
 					</Grid>
@@ -45,11 +84,31 @@ const CreateRecipePage = props => {
 					<Grid item className={classes.divider}>
 						<Divider />
 					</Grid>
-					<Grid item>
-						<p>helloo</p>
+					<Grid item className={classes.recipeDetailsRoot}>
+						<Grid
+							container
+							direction='row'
+							className={classes.recipeDetailsContainer}
+						>
+							<Grid item className={classes.instructColumn}>
+								<RecipeInstructColumn label='Ingredients' array={ingredients} />
+							</Grid>
+							<Grid item>
+								<Divider
+									orientation='vertical'
+									style={{ backgroundColor: '#000' }}
+								/>
+							</Grid>
+							<Grid item className={classes.instructColumn}>
+								<RecipeInstructColumn label='Directions' array={directions} />
+							</Grid>
+						</Grid>
 					</Grid>
 					<Grid item className={classes.divider}>
 						<Divider />
+					</Grid>
+					<Grid item>
+						<Button className={classes.saveRecipeButton}>Save Recipe</Button>
 					</Grid>
 				</Grid>
 			</Paper>
