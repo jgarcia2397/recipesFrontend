2765535ea580992ae8784715c7fa4ab1b187507e
Josh Garcia <josh2397garcia@gmail.com>
2020-10-08 19:41:30 -0400

    Add hardcoded text for profile details in ProfilePage

--- recipes-fullstack/src/components/Pages/ProfilePage.js
+++ recipes-fullstack/src/components/Pages/ProfilePage.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
 import UserProfile from '../UI/UserProfile';
+import RecipeCard from '../UI/RecipeCard';
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -16,6 +19,19 @@ const useStyles = makeStyles(theme => ({
 		width: '100%',
 		overflow: 'auto',
 	},
+	profileDetailsContainer: {
+		marginTop: '40px',
+		// marginLeft: '150px',
+		// marginRight: '150px',
+	},
+	profileDetails: {
+		paddingLeft: '350px',
+		paddingRight: '350px',
+		paddingBottom: '25px',
+	},
+	recipeCardsContainer: {
+		margin: '30px auto',
+	},
 }));
 
 const ProfilePage = props => {
@@ -25,6 +41,39 @@ const ProfilePage = props => {
 		<div className={classes.root}>
 			<Paper className={classes.background} square>
 				<UserProfile />
+				<Grid
+					container
+					direction='column'
+					className={classes.profileDetailsContainer}
+				>
+					<Grid item className={classes.profileDetails}>
+						<Typography variant='h4'>About Me</Typography>
+					</Grid>
+					<Grid item className={classes.profileDetails}>
+						<Typography variant='body1'>
+							My name's Cheffff... Just kidding, my real name is up top! I love
+							to cook and Gordon Ramsey is my hero! I wanna be like him someday,
+							but I'm not a great chef like him. I can make some decent
+							scrambled eggs though!
+						</Typography>
+					</Grid>
+					<Grid item className={classes.profileDetails}>
+						<Typography variant='h4'>Favourite Things to Cook</Typography>
+					</Grid>
+					<Grid item className={classes.profileDetails}>
+						<Typography variant='body1'>
+							I like to make Idiot Sandwiches and Gordon's famous Lamb Sauce.
+						</Typography>
+					</Grid>
+					<Grid item className={classes.profileDetails}>
+						<Typography variant='h4'>Recipe Preview</Typography>
+					</Grid>
+					<Grid item className={classes.recipeCardsContainer}>
+						<RecipeCard />
+						<RecipeCard />
+						<RecipeCard />
+					</Grid>
+				</Grid>
 			</Paper>
 		</div>
 	);
