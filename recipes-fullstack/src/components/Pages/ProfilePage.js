6551b5bcc6c3c50f304cfc3e5392ce91943132ba
Josh Garcia <josh2397garcia@gmail.com>
2020-10-08 19:02:54 -0400

    Create new UserProfile functional component for ProfilePage

--- recipes-fullstack/src/components/Pages/ProfilePage.js
+++ recipes-fullstack/src/components/Pages/ProfilePage.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 
-import profile from '../../assets/blankProfile.png';
+import UserProfile from '../UI/UserProfile';
 
 const useStyles = makeStyles(theme => ({
 	root: {
@@ -18,22 +16,6 @@ const useStyles = makeStyles(theme => ({
 		width: '100%',
 		overflow: 'auto',
 	},
-	profilePic: {
-		backgroundImage: `url(${profile})`,
-		backgroundPosition: 'center',
-		backgroundSize: 'cover',
-		backgroundRepeat: 'no-repeat',
-		borderRadius: 120,
-		height: '225px',
-		width: '225px',
-	},
-	profileContainer: {
-		marginTop: '100px',
-	},
-	profileTitlesContainer: {
-		marginLeft: '55px',
-		height: '100%',
-	},
 }));
 
 const ProfilePage = props => {
@@ -42,31 +24,7 @@ const ProfilePage = props => {
 	return (
 		<div className={classes.root}>
 			<Paper className={classes.background} square>
-				<Grid
-					container
-					direction='row'
-					justify='center'
-					className={classes.profileContainer}
-				>
-					<Grid item>
-						<Paper className={classes.profilePic} />
-					</Grid>
-					<Grid item>
-						<Grid
-							container
-							direction='column'
-							justify='center'
-							className={classes.profileTitlesContainer}
-						>
-							<Grid item>
-								<Typography variant='h3'>Joshua Garcia</Typography>
-							</Grid>
-							<Grid item>
-								<Typography variant='h4'>Master Chef</Typography>
-							</Grid>
-						</Grid>
-					</Grid>
-				</Grid>
+				<UserProfile />
 			</Paper>
 		</div>
 	);
---
+++ recipes-fullstack/src/components/UI/UserProfile.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+import profile from '../../assets/blankProfile.png';
+
+const useStyles = makeStyles(theme => ({
+	profilePic: {
+		backgroundImage: `url(${profile})`,
+		backgroundPosition: 'center',
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		borderRadius: 120,
+		height: '225px',
+		width: '225px',
+	},
+	profileContainer: {
+		marginTop: '100px',
+	},
+	profileTitlesContainer: {
+		marginLeft: '55px',
+		height: '100%',
+	},
+}));
+
+const UserProfile = props => {
+    const classes = useStyles();
+
+	return (
+		<Grid
+			container
+			direction='row'
+			justify='center'
+			className={classes.profileContainer}
+		>
+			<Grid item>
+				<Paper className={classes.profilePic} />
+			</Grid>
+			<Grid item>
+				<Grid
+					container
+					direction='column'
+					justify='center'
+					className={classes.profileTitlesContainer}
+				>
+					<Grid item>
+						<Typography variant='h3'>Joshua Garcia</Typography>
+					</Grid>
+					<Grid item>
+						<Typography variant='h4'>Master Chef</Typography>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
+};
+
+export default UserProfile;
