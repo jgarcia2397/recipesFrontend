f81d32322013aee32b1574f0036c25e24cb68622
Josh Garcia <josh2397garcia@gmail.com>
2020-10-25 15:11:06 -0400

    Create initial Modal component for adding new ingredients/directions

---
+++ recipes-fullstack/src/components/UI/Modal.js
@@ -0,0 +1,90 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+	modalTitleContainer: {
+		marginBottom: '15px',
+	},
+	contentContainer: {
+		paddingLeft: '2.5em',
+		paddingRight: '2.5em',
+	},
+	textInput: {
+		width: '100%',
+	},
+	confirmButton: {
+		...theme.typography.button,
+		borderRadius: 10,
+		backgroundColor: theme.palette.primary.main,
+		'&:hover': {
+			backgroundColor: theme.palette.primary.dark,
+		},
+		// [theme.breakpoints.down('md')]: {
+		// 	marginBottom: '20px',
+		// },
+	},
+	cancelButton: {
+		...theme.typography.button,
+		borderRadius: 10,
+		backgroundColor: theme.palette.error.main,
+		'&:hover': {
+			backgroundColor: theme.palette.error.dark,
+		},
+		// [theme.breakpoints.down('md')]: {
+		// 	marginBottom: '20px',
+		// },
+	},
+}));
+
+const Modal = props => {
+	const classes = useStyles();
+	const theme = useTheme();
+
+	return (
+		<Dialog
+			style={{ zIndex: theme.zIndex.modal + 2 }}
+			open={props.isOpen}
+			onClose={props.modalCloseHandler}
+			PaperProps={{
+				style: {
+					width: '150em',
+				},
+			}}
+		>
+			<DialogContent className={classes.contentContainer}>
+				<Typography variant='h4' className={classes.modalTitleContainer}>
+					Add New Ingredient
+				</Typography>
+				<TextField
+					id='newEntry'
+					variant='outlined'
+					className={classes.textInput}
+				/>
+			</DialogContent>
+			<DialogActions>
+				<Button
+					onClick={props.modalCloseHandler}
+					className={classes.cancelButton}
+				>
+					Cancel
+				</Button>
+				<Button
+					onClick={props.modalCloseHandler}
+					className={classes.confirmButton}
+				>
+					Save
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
+};
+
+export default Modal;
--- recipes-fullstack/src/components/UI/RecipeInstructColumn.js
+++ recipes-fullstack/src/components/UI/RecipeInstructColumn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
@@ -7,31 +7,42 @@ import Button from '@material-ui/core/Button';
 import AddCircleOutlinedIcon from '@material-ui/icons/AddCircleOutlined';
 
 import RecipeList from './RecipeList';
+import Modal from './Modal';
 
 const useStyles = makeStyles(theme => ({
 	titles: {
-        marginTop: '35px',
-        marginLeft: '15px',
-        fontWeight: 'bold',
-        [theme.breakpoints.down('xs')]: {
-            marginTop: '15px',
-        },
-    },
-    addButtonContainer: {
-        marginTop: '15px',
-        width: '100%',
-        textAlign: 'center',
-        [theme.breakpoints.down('xs')]: {
-            marginTop: '5px',
-        },
-    },
+		marginTop: '35px',
+		marginLeft: '15px',
+		fontWeight: 'bold',
+		[theme.breakpoints.down('xs')]: {
+			marginTop: '15px',
+		},
+	},
+	addButtonContainer: {
+		marginTop: '15px',
+		width: '100%',
+		textAlign: 'center',
+		[theme.breakpoints.down('xs')]: {
+			marginTop: '5px',
+		},
+	},
 }));
 
 const RecipeInstructColumn = props => {
-    const classes = useStyles();
-    const theme = useTheme();
+	const classes = useStyles();
+	const theme = useTheme();
 
-    const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
+
+	const modalOpenHandler = () => {
+		setIsModalOpen(true);
+	};
+
+	const modalCloseHandler = () => {
+		setIsModalOpen(false);
+	};
 
 	return (
 		<React.Fragment>
@@ -40,10 +51,11 @@ const RecipeInstructColumn = props => {
 			</Typography>
 			<RecipeList array={props.array} isNewRecipe={props.isNewRecipe} />
 			<div className={classes.addButtonContainer}>
-				<Button>
+				<Button onClick={modalOpenHandler}>
 					<AddCircleOutlinedIcon />
 				</Button>
 			</div>
+			<Modal isOpen={isModalOpen} modalCloseHandler={modalCloseHandler} />
 		</React.Fragment>
 	);
 };
