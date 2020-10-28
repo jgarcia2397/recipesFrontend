2d4cf6cd1f22c3576f8325cb0cdcec384f634108
Josh Garcia <josh2397garcia@gmail.com>
2020-10-27 20:21:14 -0400

    Install redux-thunk and setup middleware to update store

--- recipes-fullstack/src/index.js
+++ recipes-fullstack/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App';
-import reducer from './store/reducers/reducer';
+import reducer from './store/reducers/createRecipe';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
-  <Provider store={store}>
-    <App />
-  </Provider>
+	<Provider store={store}>
+		<App />
+	</Provider>
 );
 
-ReactDOM.render(
-  app,
-  document.getElementById('root')
-);
+ReactDOM.render(app, document.getElementById('root'));
