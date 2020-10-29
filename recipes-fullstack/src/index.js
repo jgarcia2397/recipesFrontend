96cbcc3a3cb157dd094b00c0d156acc77d27409e
Josh Garcia <josh2397garcia@gmail.com>
2020-10-29 18:11:38 -0400

    Create rootReducer so that useSelector hook in CreateRecipePage can access the state properly

--- recipes-fullstack/src/index.js
+++ recipes-fullstack/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 import App from './App';
-import reducer from './store/reducers/createRecipe';
+import createRecipeReducer from './store/reducers/createRecipe';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const rootReducer = combineReducers({
+	createRecipe: createRecipeReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
 	<Provider store={store}>
