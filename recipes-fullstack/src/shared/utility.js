80e232bec9fd0890791c09545d986b6e79a7c3b4
Josh Garcia <josh2397garcia@gmail.com>
2020-11-19 17:37:52 -0500

    Add shared utility function checkInputValidity() for validating form inputs

--- recipes-fullstack/src/shared/utility.js
+++ recipes-fullstack/src/shared/utility.js
@@ -4,3 +4,22 @@ export const updateObject = (oldObject, updatedProperties) => {
         ...updatedProperties
     };
 };
+
+export const checkInputValidity = (value, rules) => {
+    let isValid = true;
+
+    if (!rules) {
+        return true;
+    }
+
+    if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+};
