819aa1a35393e40de1fc9ab5d1c3af470418ea6d
Josh Garcia <josh2397garcia@gmail.com>
2021-01-17 15:34:21 -0500

    Separate authenticated and unauthenticated routes

--- recipes-frontend/src/App.js
+++ recipes-frontend/src/App.js
@@ -20,40 +20,122 @@ let logoutTimer;
 function App() {
 	const [tabValue, setTabValue] = useState(0);
 
-	const userId = useSelector(state => state.user.userId);
-
-	const routes = [
-		{ name: 'Home', link: '/', activeIndex: 0, isMainTab: true },
-		{
-			name: 'My Recipes',
-			link: `/recipes/${userId}`,
-			activeIndex: 1,
-			isMainTab: true,
-		},
-		{
-			name: 'My Profile',
-			link: `/profile/${userId}`,
-			activeIndex: 2,
-			isMainTab: true,
-		},
-		{ name: 'Log In/Out', link: '/auth', activeIndex: 3, isMainTab: true },
-		{
-			name: 'New Recipe',
-			link: '/new-recipe',
-			activeIndex: false,
-			isMainTab: false,
-		},
-		{
-			name: 'Full Recipe Details',
-			link: '/recipe-full-details',
-			activeIndex: false,
-			isMainTab: false,
-		},
-	];
-
+	// auth constants
 	const token = useSelector(state => state.user.token);
 	const tokenExpiration = useSelector(state => state.user.tokenExpiration);
+	const userId = useSelector(state => state.user.userId);
 
+	let routes;
+	if (token) {
+		routes = [
+			{ name: 'Home', link: '/', activeIndex: 0, isMainTab: true },
+			{
+				name: 'My Recipes',
+				link: `/recipes/${userId}`,
+				activeIndex: 1,
+				isMainTab: true,
+			},
+			{
+				name: 'My Profile',
+				link: `/profile/${userId}`,
+				activeIndex: 2,
+				isMainTab: true,
+			},
+			{ name: 'Log Out', link: '/auth', activeIndex: 3, isMainTab: true },
+			{
+				name: 'New Recipe',
+				link: '/new-recipe',
+				activeIndex: false,
+				isMainTab: false,
+			},
+			{
+				name: 'Full Recipe Details',
+				link: '/recipe-full-details',
+				activeIndex: false,
+				isMainTab: false,
+			},
+		];
+	} else {
+		routes = [
+			{ name: 'Home', link: '/', activeIndex: 0, isMainTab: true },
+			{ name: 'Log In', link: '/auth', activeIndex: 1, isMainTab: true },
+		];
+	}
+
+	let routeComponents;
+	if (token) {
+		routeComponents = (
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={() => <HomePage routes={routes} setTabValue={setTabValue} />}
+				/>
+				<Route
+					exact
+					path='/auth'
+					render={() => <Auth routes={routes} setTabValue={setTabValue} />}
+				/>
+				<Route
+					exact
+					path='/recipes/:userId'
+					render={() => (
+						<RecipePage
+							tabValue={tabValue}
+							routes={routes}
+							setTabValue={setTabValue}
+						/>
+					)}
+				/>
+				<Route
+					exact
+					path='/recipe-full-details'
+					render={props => (
+						<RecipeFullDetailsPage
+							{...props}
+							routes={routes}
+							setTabValue={setTabValue}
+						/>
+					)}
+				/>
+				<Route
+					exact
+					path='/new-recipe'
+					render={props => (
+						<CreateRecipePage
+							{...props}
+							routes={routes}
+							setTabValue={setTabValue}
+						/>
+					)}
+				/>
+				<Route
+					exact
+					path='/profile/:userId'
+					render={() => (
+						<ProfilePage routes={routes} setTabValue={setTabValue} />
+					)}
+				/>
+			</Switch>
+		);
+	} else {
+		routeComponents = (
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={() => <HomePage routes={routes} setTabValue={setTabValue} />}
+				/>
+				<Route
+					exact
+					path='/auth'
+					render={() => <Auth routes={routes} setTabValue={setTabValue} />}
+				/>
+			</Switch>
+		);
+	}
+
+	// ** AUTHENTICATION **
 	const dispatch = useDispatch();
 
 	const onAutoLogin = useCallback(
@@ -68,7 +150,8 @@ function App() {
 
 	useEffect(() => {
 		if (token && tokenExpiration) {
-			const remainingTime = new Date(tokenExpiration).getTime() - new Date().getTime();
+			const remainingTime =
+				new Date(tokenExpiration).getTime() - new Date().getTime();
 			logoutTimer = setTimeout(onAutoLogout, remainingTime);
 		} else {
 			clearTimeout(logoutTimer);
@@ -89,65 +172,13 @@ function App() {
 			);
 		}
 	}, [onAutoLogin]);
+	// ** AUTHENTICATION **
 
 	return (
 		<ThemeProvider theme={theme}>
 			<BrowserRouter>
 				<Header routes={routes} tabValue={tabValue} setTabValue={setTabValue} />
-				<Switch>
-					<Route
-						exact
-						path='/'
-						render={() => (
-							<HomePage routes={routes} setTabValue={setTabValue} />
-						)}
-					/>
-					<Route
-						exact
-						path='/auth'
-						render={() => <Auth routes={routes} setTabValue={setTabValue} />}
-					/>
-					<Route
-						exact
-						path='/recipes/:userId'
-						render={() => (
-							<RecipePage
-								tabValue={tabValue}
-								routes={routes}
-								setTabValue={setTabValue}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/recipe-full-details'
-						render={props => (
-							<RecipeFullDetailsPage
-								{...props}
-								routes={routes}
-								setTabValue={setTabValue}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/new-recipe'
-						render={props => (
-							<CreateRecipePage
-								{...props}
-								routes={routes}
-								setTabValue={setTabValue}
-							/>
-						)}
-					/>
-					<Route
-						exact
-						path='/profile/:userId'
-						render={() => (
-							<ProfilePage routes={routes} setTabValue={setTabValue} />
-						)}
-					/>
-				</Switch>
+				{routeComponents}
 			</BrowserRouter>
 		</ThemeProvider>
 	);
